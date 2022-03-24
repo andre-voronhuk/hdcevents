237d4f7b306aa5cc9e4d0c924ea9f004bd8c34a5
André Voronhuk <andrevoronhuk@gmail.com>
2022-03-24 12:49:55 -0300

    Aula 7
    + Font mudada
    + Imagens
    + arquivo JS
    + arquivo CSS
    + layout main padrao
    + adequacao das paginas {welcome e produtos} para usarem o layout padrao
    + import de font e Bootstrap
    - Reset Css do template

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>@yield('title')</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto" rel="stylesheet">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <!-- Styles -->
+    <link rel="stylesheet" href="/css/styles.css">
+    <script src="/js/scripts.js"></script>
+
+
+
+    <style>
+        body {
+            color: rgb(255, 255, 255, 0.87);
+            background-color: #222223;
+        }
+
+    </style>
+</head>
+
+<body class="antialiased">
+
+    @yield('content')
+
+    <footer>
+        <p>HDC Events &copy; 2022</p>
+    </footer>
+
+</body>
+
+</html>

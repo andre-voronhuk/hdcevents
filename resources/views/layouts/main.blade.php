97dd6a454c49ca3230ee9c55fb7116260fb018d7
André Voronhuk <andrevoronhuk@gmail.com>
2022-03-26 18:10:56 -0300

    Criação do Nav Bar

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -21,19 +21,40 @@
     <link rel="stylesheet" href="/css/styles.css">
     <script src="/js/scripts.js"></script>
 
-
-
-    <style>
-        body {
-            color: rgb(255, 255, 255, 0.87);
-            background-color: #222223;
-        }
-
-    </style>
 </head>
 
 <body class="antialiased">
-
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-light">
+            <div class="collapse navbar-collapse" id="navbar">
+                <a href="/" class="navbar-brand">
+                    <img src="/img/hdcevents_logo.svg" alt="" srcset="">
+                </a>
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a href="/" class="nav-link">
+                            Eventos
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="/" class="nav-link">
+                            Criar Eventos
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="/" class="nav-link">
+                            Entrar
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="/" class="nav-link">
+                            Cadastrar
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </header>
     @yield('content')
 
     <footer>

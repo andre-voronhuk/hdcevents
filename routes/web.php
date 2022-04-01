1bc31bd7a9106b8da2dd7fa760563f7d0eab41da
André Voronhuk <andrevoronhuk@gmail.com>
2022-03-31 23:32:37 -0300

    removido testes

--- routes/web.php
+++ routes/web.php
@@ -20,14 +20,3 @@ Route::get('/events/create', [EventController::class, 'create']);
 Route::get('/contato', function () {
     return view('contact');
 });
-
-Route::get('/produtos', function () {
-
-    $busca = request('search');
-
-    return view('products', ['busca' => $busca]);
-});
-
-Route::get('/produtos_teste/{id?}', function ($id = null) {
-    return view('product', ['id' => $id]);
-});

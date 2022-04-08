b7546515227af27c7ddd7ffc66731a4ba422c88c
André Voronhuk <andrevoronhuk@gmail.com>
2022-04-08 20:26:13 -0300

    mudanças visuais na pagina inciar

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,16 +4,45 @@
 
 @section('content')
 
-    <h4>
 
 
-        @foreach ($events as $event)
-            <h2>
-                #{{ $event['id'] }}:
-                {{ $event['title'] }}</h2>
-            <h4>{{ $event['description'] }} </h4> <br>
-        @endforeach
 
-    </h4>
+    <div id="search-container" class="col-md-12">
+        <h1>Busque um evento</h1>
+
+        <form action="">
+            <input type="text" id="search" name="search" class="form-control" placeholder="Procurar...">
+
+        </form>
+    </div>
+    <div id="events-container" class="col-md-12">
+        <h2>Proximos Eventos</h2>
+        <p>Veja os eventos dos próximos dias</p>
+
+        <div id="cards-container" class="row">
+            @foreach ($events as $event)
+                <div class="card col-md-3">
+                    <img src="/img/event_placeholder.jpg" alt="{{ $event->title }}">
+
+                    <div class="card-body">
+                        <p class="card-date">
+                            10/10/2022
+                        </p>
+                        <h5 class="card-title">
+                            {{ $event->title }}
+                        </h5>
+                        <p class="card-participants">
+
+                        </p>
+                        <a href="#" class="btn btn-primary">
+                            Participar
+                        </a>
+                    </div>
+                </div>
+            @endforeach
+
+        </div>
+    </div>
+
 
 @endsection

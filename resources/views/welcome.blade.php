aa9a9fd8e5a52e982a04a8ea2ec49ffffdfc3084
André Voronhuk <andrevoronhuk@gmail.com>
2022-04-11 13:10:24 -0300

    mudanças no estilo
    - wrapper das imagens
    + css geral na pagina welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,8 +9,8 @@
 
     <div id="search-container" class="col-md-12">
         <h1>Busque um evento</h1>
-
         <form action="">
+
             <input type="text" id="search" name="search" class="form-control" placeholder="Procurar...">
 
         </form>
